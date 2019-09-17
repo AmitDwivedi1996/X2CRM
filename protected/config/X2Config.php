8fe2b7333471c6b11b892c88d9724052e01108a3
x2crm <ktd.amitdwivedi@gmail.com>
2019-09-18 01:22:16 +0530

    This is First Commit

---
+++ protected/config/X2Config.php
@@ -0,0 +1,12 @@
+<?php
+$appName = 'X2CRM';
+$email = 'amit@gmail.com';
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$dbname = 'x2engine';
+$version = '6.6';
+$buildDate = 1494966774;
+$updaterVersion = '6.0.3';
+$language='en';
+?>
